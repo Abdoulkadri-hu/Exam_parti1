39e7e4e24bf3418f098128c7efbd7a0b0f6acee6
Abdoulkadri-hu <bakoabdoulkadri@gmail.com>
2025-05-11 01:23:27 +0100

    hello_world.sh

--- hello_world.sh
+++ hello_world.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-printf 'hello, world!' # printf pour affiche le texte "Hello World!" à l’écran.
+printf 'hello, world!' #printf pour affiche le texte "Hello World!" à l’écran.
+echo hello World #echo aussi permet d'afficher à l'écran 
